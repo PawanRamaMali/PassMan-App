60e84122cce915e690f3813cb1e4d7524d4db894
Pawan Rama Mali <prm@outlook.in>
2021-10-22 23:29:38 +0530

    added hashing functionality

--- server.R
+++ server.R
@@ -3,5 +3,8 @@ server <- function(input, output, session) {
   # * Login Functionality ----
   source('./srv/login.R', local = TRUE)
   
+  # * Hashing Functionality ----
+  source('./srv/hash.R', local = TRUE)
+  
   
 }
--- ui/admin/build_page.R
+++ ui/admin/build_page.R
@@ -3,37 +3,51 @@
 tabPanel("BUILD",
          value = "Build",
          fluidPage
-         (
-           
-           fluidRow(
-           
+         (fluidRow(
            h3("Build you Hashed and Salted Password"),
            
-           selectInput(
-             inputId="select_hash",
-             label="Select Hash Function",
-             choices=c("md5", "sha1", "crc32", "sha256", "sha512",
-                       "xxhash32", "xxhash64", "murmur32", "spookyhash",
-                       "blake3"),
-             selected = "sha512",
-             multiple = FALSE
+           column(
+             2,
+             selectInput(
+               inputId = "select_hash",
+               label = "Select Hash Function",
+               choices = c(
+                 "md5",
+                 "sha1",
+                 "crc32",
+                 "sha256",
+                 "sha512",
+                 "xxhash32",
+                 "xxhash64",
+                 "murmur32",
+                 "spookyhash",
+                 "blake3"
+               ),
+               selected = "sha512",
+               multiple = FALSE
+             ),
            ),
-           actionButton(inputId = "start_hash",
-                        label="Submit",
-                        icon = NULL,
-                        width = "100%")
            
+           column(
+             2,
+             textInput(
+               inputId = "text_to_hash",
+               value = "",
+               label = "Enter Password to hash : "
+             ),
+             
+             actionButton(
+               inputId = "start_hash",
+               label = "Submit",
+               icon = NULL,
+               width = "100%",
+               class = "btn-success"
+             )
+           ),
+           column(3,
+                  fluidRow(
+                    verbatimTextOutput(outputId = "hashed_output",
+                                       placeholder = FALSE)
+                  ))
            
-           
-           
-         ),
-           
-           fluidRow(
-             verbatimTextOutput(
-               outputId="hashed_output",
-               placeholder = FALSE)
-           )
-           
-           
-           
-           ))
+         )))
