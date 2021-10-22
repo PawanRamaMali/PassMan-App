82fa51b4c330ee50cb32f9aedeee8a5f52741d86
Pawan Rama Mali <prm@outlook.in>
2021-10-22 22:41:03 +0530

    Update build_page.R

--- ui/admin/build_page.R
+++ ui/admin/build_page.R
@@ -3,6 +3,37 @@
 tabPanel("BUILD",
          value = "Build",
          fluidPage
-         (fluidRow(
-           "BUILD"
-         )))
+         (
+           
+           fluidRow(
+           
+           h3("Build you Hashed and Salted Password"),
+           
+           selectInput(
+             inputId="select_hash",
+             label="Select Hash Function",
+             choices=c("md5", "sha1", "crc32", "sha256", "sha512",
+                       "xxhash32", "xxhash64", "murmur32", "spookyhash",
+                       "blake3"),
+             selected = "sha512",
+             multiple = FALSE
+           ),
+           actionButton(inputId = "start_hash",
+                        label="Submit",
+                        icon = NULL,
+                        width = "100%")
+           
+           
+           
+           
+         ),
+           
+           fluidRow(
+             verbatimTextOutput(
+               outputId="hashed_output",
+               placeholder = FALSE)
+           )
+           
+           
+           
+           ))
